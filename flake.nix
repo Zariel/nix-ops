12f36c7eb7d385402beab0ec443a843e510ad036
Chris Bannister <c.bannister@gmail.com>
2025-10-10 21:41:19 +0100

    deploy-rs

--- flake.nix
+++ flake.nix
@@ -6,6 +6,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -13,6 +18,7 @@
       self,
       nixpkgs,
       home-manager,
+      deploy-rs,
       ...
     }@inputs:
     {
@@ -33,5 +39,16 @@
           ];
         };
       };
+
+      deploy.nodes.dns1 = {
+        hostname = "10.1.53.10";
+        profiles.system = {
+          sshUser = "chris";
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.dns1;
+        };
+      };
+
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
 }
