7c6e6d825f9569ac8012419fff13e24ba8a62c6c
Chris Bannister <c.bannister@gmail.com>
2026-01-12 21:06:22 +0000

    dev; add nfs-utils

--- systems/dev/default.nix
+++ systems/dev/default.nix
@@ -146,6 +146,7 @@
     lsof
     usbutils
     fio
+    nfs-utils
     # inputs.nixpkgs-gamma.legacyPackages.${pkgs.system}.gamma-launcher
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
