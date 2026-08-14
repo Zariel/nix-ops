9488e030672c3c3846a6d430b559651eaa454fd6
Chris Bannister <c.bannister@gmail.com>
2026-08-14 23:07:37 +0100

    roles/base: garnix no more

--- systems/thinliz/default.nix
+++ systems/thinliz/default.nix
@@ -229,14 +229,12 @@ in
         "https://nix-community.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
         "https://cachix.cachix.org"
-        "https://cache.garnix.io"
       ];
 
       trusted-public-keys = lib.mkAfter [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "cache-name:nKgRvz/pXDZWsXAuzXcoRyyW2Ryut5EpoeLEeiyqgnA="
       ];
     };
